c9c02c97f2837ccdb4cd4be882682f44b652c5c2
yashmerchant23 <merchantyash45@gmail.com>
2024-07-11 20:33:11 +0530

    avoiding warnings

--- image_to_text.py
+++ image_to_text.py
@@ -1,4 +1,35 @@
+# from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# # Define the Hugging Face model path
+# hf_path = 'jiajunlong/TinyLLaVA-OpenELM-450M-CLIP-0.55B'
+
+# # Load the model
+# model = AutoModelForCausalLM.from_pretrained(hf_path, trust_remote_code=True)
+# config = model.config
+
+# # Load the tokenizer
+# tokenizer = AutoTokenizer.from_pretrained(hf_path, use_fast=False, model_max_length=config.max_length)
+
+# # Define prompt and image URL
+# prompt = "Describe the image. descriptions should be concise yet vivid, highlighting any notable features or behaviors. Try to keep it lighthearted and entertaining, but avoid repeating yourself. Remember to point out anything particularly interesting!"
+# image_url = "soccer_practice.jpg"
+
+
+# # Generate text based on prompt and image
+# output_text, generation_time = model.chat(prompt=prompt, image=image_url, tokenizer=tokenizer)
+
+# # Remove any unwanted newlines or extra spaces
+# cleaned_output_text = output_text.replace('\n', ' ').strip()
+
+# print('Cleaned model output:', cleaned_output_text)
+# print('Running time:', generation_time)
+
+
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import warnings 
+
+warnings.simplefilter(action = 'ignore' , category =FutureWarning)
+warnings.simplefilter(action='ignore', category=UserWarning)
 
 # Define the Hugging Face model path
 hf_path = 'jiajunlong/TinyLLaVA-OpenELM-450M-CLIP-0.55B'
@@ -14,15 +45,19 @@ tokenizer = AutoTokenizer.from_pretrained(hf_path, use_fast=False, model_max_len
 prompt = "Describe the image. descriptions should be concise yet vivid, highlighting any notable features or behaviors. Try to keep it lighthearted and entertaining, but avoid repeating yourself. Remember to point out anything particularly interesting!"
 image_url = "soccer_practice.jpg"
 
+# Truncate the prompt if it exceeds the maximum length
+truncated_prompt = prompt[:config.max_length]
+
+# Enable sampling if using temperature
+model.config.do_sample = True  # Enable sampling
+model.config.temperature = 1.0  # Set temperature (adjust as needed)
 
 # Generate text based on prompt and image
-output_text, generation_time = model.chat(prompt=prompt, image=image_url, tokenizer=tokenizer)
+output_text, generation_time = model.chat(prompt=truncated_prompt, image=image_url, tokenizer=tokenizer)
 
-# Remove any unwanted newlines or extra spaces
+
+# Clean and print the output
 cleaned_output_text = output_text.replace('\n', ' ').strip()
-
 print('Cleaned model output:', cleaned_output_text)
 print('Running time:', generation_time)
-
-
 
