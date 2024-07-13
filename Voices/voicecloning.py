8b7a21a8a84ad073e1cfe8b76cb12033547194aa
yashmerchant23 <merchantyash45@gmail.com>
2024-07-13 16:59:51 +0530

    adding new voices

--- Voices/voicecloning.py
+++ Voices/voicecloning.py
@@ -1,10 +1,12 @@
+
+
 from TTS.api import TTS
 tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
 
 # generate speech by cloning a voice using default settings
 tts.tts_to_file(text="I am king khan. the badshah of bollywood and the owner of Mannat",
                 file_path="output.wav",
-                speaker_wav=["srk_35_sec.wav"],
+                speaker_wav=["obama_30_sec.wav"],
                 language="en",
                 split_sentences=True
                 )
