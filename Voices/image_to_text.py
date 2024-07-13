4319c2942ea906b97e2bf432ae78be7e9ba1c35e
yashmerchant23 <merchantyash45@gmail.com>
2024-07-13 13:10:25 +0530

    including text generation in the venv

---
+++ Voices/image_to_text.py
@@ -0,0 +1,59 @@
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import time
+import os
+import cv2
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=UserWarning)
+
+# Define the Hugging Face model path
+hf_path = 'jiajunlong/TinyLLaVA-OpenELM-450M-CLIP-0.55B'
+
+# Load the model
+model = AutoModelForCausalLM.from_pretrained(hf_path, trust_remote_code=True)
+config = model.config
+
+# Load the tokenizer
+tokenizer = AutoTokenizer.from_pretrained(hf_path, use_fast=False, model_max_length=config.max_length)
+
+# Define prompt and image path
+prompt = "Describe the image in very funny way. Be very accurate. do not repeat anything you said previously. "
+image_path = "../frames/frame.jpg"
+
+# Truncate the prompt if it exceeds the maximum length
+truncated_prompt = prompt[:config.max_length]
+
+# Function to read the latest image and generate text
+def generate_text_for_latest_image():
+    while True:
+        try:
+            # Check if the image file exists
+            if os.path.exists(image_path):
+                # Load the image
+                frame = cv2.imread(image_path)
+
+                # Generate text based on the image
+                output_text, generation_time = model.chat(prompt=truncated_prompt, image=image_path, tokenizer=tokenizer)
+
+                # Clean and print the output
+                cleaned_output_text = output_text.replace('\n', ' ').strip()
+                print('Cleaned model output:', cleaned_output_text)
+                print('Running time:', generation_time)
+
+                # Sleep for a moment before generating text again
+                time.sleep(1)
+        
+        except KeyboardInterrupt:
+            print("Stopping the script...")
+            break
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            time.sleep(5)
+
+# Run the function to continuously generate text for the latest image
+generate_text_for_latest_image()
+
+# Clean up
+cv2.destroyAllWindows()
--- Voices/voicecloning.py
+++ Voices/voicecloning.py
@@ -2,9 +2,10 @@ from TTS.api import TTS
 tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
 
 # generate speech by cloning a voice using default settings
-tts.tts_to_file(text="It took me quite a long time to develop a voice, and now that I have it I'm not going to be silent.",
+tts.tts_to_file(text="I am king khan. the badshah of bollywood and the owner of Mannat",
                 file_path="output.wav",
-                speaker_wav=["trump_30_sec.wav"],
+                speaker_wav=["srk_35_sec.wav"],
                 language="en",
                 split_sentences=True
                 )
+
